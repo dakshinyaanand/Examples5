79aff74938b36aac304a4f0ce5688e92f364a547
nexwave <nexwave@DESKTOP-P7TRHT5>
2018-07-20 01:09:14 -0700

    Add project files.

---
+++ DisconnectedExample/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace DisconnectedExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnselect = new System.Windows.Forms.Button();
+            this.btnupdate = new System.Windows.Forms.Button();
+            this.btnxml = new System.Windows.Forms.Button();
+            this.dgselect = new System.Windows.Forms.DataGridView();
+            this.lbxml = new System.Windows.Forms.ListBox();
+            this.lblid = new System.Windows.Forms.Label();
+            this.txtid = new System.Windows.Forms.TextBox();
+            this.btnsp = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.trainingDBDataSet = new DisconnectedExample.TrainingDBDataSet();
+            this.registerShoppingBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.registerShoppingTableAdapter = new DisconnectedExample.TrainingDBDataSetTableAdapters.RegisterShoppingTableAdapter();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.genderDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.membershipDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.shoppingPreferenceDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cODDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.passwordDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgselect)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trainingDBDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.registerShoppingBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnselect
+            // 
+            this.btnselect.Location = new System.Drawing.Point(350, 69);
+            this.btnselect.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnselect.Name = "btnselect";
+            this.btnselect.Size = new System.Drawing.Size(112, 28);
+            this.btnselect.TabIndex = 0;
+            this.btnselect.Text = "Select";
+            this.btnselect.UseVisualStyleBackColor = true;
+            this.btnselect.Click += new System.EventHandler(this.btnselect_Click);
+            // 
+            // btnupdate
+            // 
+            this.btnupdate.Location = new System.Drawing.Point(553, 69);
+            this.btnupdate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnupdate.Name = "btnupdate";
+            this.btnupdate.Size = new System.Drawing.Size(112, 28);
+            this.btnupdate.TabIndex = 1;
+            this.btnupdate.Text = "Update";
+            this.btnupdate.UseVisualStyleBackColor = true;
+            this.btnupdate.Click += new System.EventHandler(this.btnupdate_Click);
+            // 
+            // btnxml
+            // 
+            this.btnxml.Location = new System.Drawing.Point(756, 69);
+            this.btnxml.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnxml.Name = "btnxml";
+            this.btnxml.Size = new System.Drawing.Size(112, 28);
+            this.btnxml.TabIndex = 2;
+            this.btnxml.Text = "Show XML";
+            this.btnxml.UseVisualStyleBackColor = true;
+            this.btnxml.Click += new System.EventHandler(this.btnxml_Click);
+            // 
+            // dgselect
+            // 
+            this.dgselect.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgselect.Location = new System.Drawing.Point(131, 151);
+            this.dgselect.Name = "dgselect";
+            this.dgselect.Size = new System.Drawing.Size(603, 290);
+            this.dgselect.TabIndex = 3;
+            this.dgselect.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgselect_CellContentClick);
+            // 
+            // lbxml
+            // 
+            this.lbxml.FormattingEnabled = true;
+            this.lbxml.ItemHeight = 16;
+            this.lbxml.Location = new System.Drawing.Point(836, 208);
+            this.lbxml.Name = "lbxml";
+            this.lbxml.Size = new System.Drawing.Size(218, 244);
+            this.lbxml.TabIndex = 4;
+            // 
+            // lblid
+            // 
+            this.lblid.AutoSize = true;
+            this.lblid.Location = new System.Drawing.Point(817, 151);
+            this.lblid.Name = "lblid";
+            this.lblid.Size = new System.Drawing.Size(61, 16);
+            this.lblid.TabIndex = 5;
+            this.lblid.Text = "Enter Id";
+            // 
+            // txtid
+            // 
+            this.txtid.Location = new System.Drawing.Point(916, 146);
+            this.txtid.Name = "txtid";
+            this.txtid.Size = new System.Drawing.Size(100, 22);
+            this.txtid.TabIndex = 6;
+            // 
+            // btnsp
+            // 
+            this.btnsp.Location = new System.Drawing.Point(1049, 151);
+            this.btnsp.Name = "btnsp";
+            this.btnsp.Size = new System.Drawing.Size(75, 23);
+            this.btnsp.TabIndex = 7;
+            this.btnsp.Text = "Call SP";
+            this.btnsp.UseVisualStyleBackColor = true;
+            this.btnsp.Click += new System.EventHandler(this.btnsp_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.nameDataGridViewTextBoxColumn,
+            this.genderDataGridViewTextBoxColumn,
+            this.membershipDataGridViewTextBoxColumn,
+            this.shoppingPreferenceDataGridViewTextBoxColumn,
+            this.cODDataGridViewTextBoxColumn,
+            this.passwordDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.registerShoppingBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(131, 448);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(603, 281);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // trainingDBDataSet
+            // 
+            this.trainingDBDataSet.DataSetName = "TrainingDBDataSet";
+            this.trainingDBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // registerShoppingBindingSource
+            // 
+            this.registerShoppingBindingSource.DataMember = "RegisterShopping";
+            this.registerShoppingBindingSource.DataSource = this.trainingDBDataSet;
+            // 
+            // registerShoppingTableAdapter
+            // 
+            this.registerShoppingTableAdapter.ClearBeforeFill = true;
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "Id";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            this.idDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "Name";
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            // 
+            // genderDataGridViewTextBoxColumn
+            // 
+            this.genderDataGridViewTextBoxColumn.DataPropertyName = "Gender";
+            this.genderDataGridViewTextBoxColumn.HeaderText = "Gender";
+            this.genderDataGridViewTextBoxColumn.Name = "genderDataGridViewTextBoxColumn";
+            // 
+            // membershipDataGridViewTextBoxColumn
+            // 
+            this.membershipDataGridViewTextBoxColumn.DataPropertyName = "Membership";
+            this.membershipDataGridViewTextBoxColumn.HeaderText = "Membership";
+            this.membershipDataGridViewTextBoxColumn.Name = "membershipDataGridViewTextBoxColumn";
+            // 
+            // shoppingPreferenceDataGridViewTextBoxColumn
+            // 
+            this.shoppingPreferenceDataGridViewTextBoxColumn.DataPropertyName = "ShoppingPreference";
+            this.shoppingPreferenceDataGridViewTextBoxColumn.HeaderText = "ShoppingPreference";
+            this.shoppingPreferenceDataGridViewTextBoxColumn.Name = "shoppingPreferenceDataGridViewTextBoxColumn";
+            // 
+            // cODDataGridViewTextBoxColumn
+            // 
+            this.cODDataGridViewTextBoxColumn.DataPropertyName = "COD";
+            this.cODDataGridViewTextBoxColumn.HeaderText = "COD";
+            this.cODDataGridViewTextBoxColumn.Name = "cODDataGridViewTextBoxColumn";
+            // 
+            // passwordDataGridViewTextBoxColumn
+            // 
+            this.passwordDataGridViewTextBoxColumn.DataPropertyName = "Password";
+            this.passwordDataGridViewTextBoxColumn.HeaderText = "Password";
+            this.passwordDataGridViewTextBoxColumn.Name = "passwordDataGridViewTextBoxColumn";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lavender;
+            this.ClientSize = new System.Drawing.Size(1599, 583);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnsp);
+            this.Controls.Add(this.txtid);
+            this.Controls.Add(this.lblid);
+            this.Controls.Add(this.lbxml);
+            this.Controls.Add(this.dgselect);
+            this.Controls.Add(this.btnxml);
+            this.Controls.Add(this.btnupdate);
+            this.Controls.Add(this.btnselect);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgselect)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trainingDBDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.registerShoppingBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnselect;
+        private System.Windows.Forms.Button btnupdate;
+        private System.Windows.Forms.Button btnxml;
+        private System.Windows.Forms.DataGridView dgselect;
+        private System.Windows.Forms.ListBox lbxml;
+        private System.Windows.Forms.Label lblid;
+        private System.Windows.Forms.TextBox txtid;
+        private System.Windows.Forms.Button btnsp;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private TrainingDBDataSet trainingDBDataSet;
+        private System.Windows.Forms.BindingSource registerShoppingBindingSource;
+        private TrainingDBDataSetTableAdapters.RegisterShoppingTableAdapter registerShoppingTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn genderDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn membershipDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn shoppingPreferenceDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cODDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn passwordDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ DisconnectedExample/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace DisconnectedExample
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con = null;
+        SqlDataAdapter adp = null;
+        DataSet ds = null;
+        string constr = "Data Source=(local);Initial Catalog=TrainingDB;Integrated Security=True;Pooling=False";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnupdate_Click(object sender, EventArgs e)
+        {
+           int res= adp.Update(ds.Tables["shop"]);
+            if (res > 1)
+            {
+                MessageBox.Show("data updated");
+            }
+        }
+
+        private void btnselect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                con = new SqlConnection(constr);
+                string query = "select * from Registershopping";
+                adp = new SqlDataAdapter(query, con);
+                ds = new DataSet();
+                adp.Fill(ds, "shop");
+                SqlCommandBuilder builder = new SqlCommandBuilder(adp);
+                adp.InsertCommand=builder.GetInsertCommand();
+               adp.UpdateCommand= builder.GetUpdateCommand();
+               adp.DeleteCommand= builder.GetDeleteCommand();
+               // MessageBox.Show(adp.InsertCommand.CommandText);
+                //MessageBox.Show(adp.UpdateCommand.CommandText);
+                //MessageBox.Show(adp.DeleteCommand.CommandText);
+
+                dgselect.DataSource = ds.Tables["shop"];
+            }
+            catch (SqlException sql)
+            {
+                MessageBox.Show(sql.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private void dgselect_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnxml_Click(object sender, EventArgs e)
+        {
+            lbxml.Items.Clear();
+           MessageBox.Show(ds.GetXml());
+
+        }
+
+        private void btnsp_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                con = new SqlConnection(constr);
+                con.Open();
+                SqlCommand cmd = new SqlCommand("sp_SelectRegisterShopping", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                int id = int.Parse(txtid.Text);
+                cmd.Parameters.AddWithValue("@id", id);
+                var r = cmd.ExecuteReader();
+                if (r.Read())
+                {
+                    for (int i = 0;i< r.FieldCount; i++){
+                        lbxml.Items.Add(r[i]);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("invalid");
+                }
+            }
+            catch (SqlException sql)
+            {
+                MessageBox.Show(sql.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'trainingDBDataSet.RegisterShopping' table. You can move, or remove it, as needed.
+            this.registerShoppingTableAdapter.Fill(this.trainingDBDataSet.RegisterShopping);
+
+        }
+    }
+}
